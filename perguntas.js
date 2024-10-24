99bfcd00f33b27f5c3e79645077606ae6b9201a3
7heWatter <147631009+7heWatter@users.noreply.github.com>
2024-10-24 14:56:41 -0300

    24/10

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Quais países fazem fronteira com a Eslovênia?',
+    'Áustria, Croácia, Hungria, Itália.',
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual o planeta conhecido como super saturno?',
+    'J1407b.',
+)
+
+criaCartao(
+    'Astronomia',
+    'O que tem na superfíce de gliese 436b?',
+    'água, porém, em um estado incomum: gelo quente.',
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o território autônomo da Dinamarca conhecido por suas montanhas dramáticas e sua ausência de árvores?',
+    'Ilhas faroe',
+)
